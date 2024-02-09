d0b13162e205f4922232646bace62072f72a08e4
viktor <vharatyk@c1r6p9.42lehavre.fr>
2024-02-09 17:18:01 +0100

    finish

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:39:58 by vharatyk          #+#    #+#             */
-/*   Updated: 2024/02/06 14:02:42 by vharatyk         ###   ########.fr       */
+/*   Updated: 2024/02/09 14:48:37 by vharatyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static long	size_max(long long result)
 {
-	if (result > 2147483647 || result < -2147483647)
+	if (result > 2147483647 || result < -2147483648)
 	{
 		return (2147483649);
 	}
@@ -41,11 +41,11 @@ long long	ft_atoi(const char *str )
 	if (str[i] == '-' || str[i] == '+')
 		return (0);
 	while (str[i] >= '0' && str[i] <= '9')
-	{
 		result = result * 10 + str[i++] - '0';
-		result = size_max(result);
-	}
-	return (result * value_oper);
+	if (value_oper == -1)
+		return (size_max(-1 * result));
+	else
+		return (size_max(result));
 }
 
 int	get_distance_node(t_node **stack, int index)
